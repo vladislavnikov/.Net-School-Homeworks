31b79b98e5d92669288c9055d42fbcfe57001b1c
Vladislav <vladislav.d.nikov04@gmail.com>
2024-05-24 16:27:51 +0300

    HM3

---
+++ HM3/Task3.1/Helper.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Task3._1
+{
+    public static class Helper
+    {
+        public static IQueue<T> Tail<T>(this IQueue<T> queue)
+        {
+            if (queue.isEmpty())
+            {
+                throw new InvalidOperationException("Empty queue!");
+            }
+
+            IQueue<T> newQueue = new Queue<T>();
+
+            queue.Dequeue();
+
+            while (!queue.isEmpty())
+            {
+                newQueue.Enqueue(queue.Dequeue());
+            }
+
+            return newQueue;
+        }
+    }
+}
