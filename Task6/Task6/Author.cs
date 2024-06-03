8b0d9c08dc6a720eae35c9cbbc9253914e30930a
Vladislav <vladislav.d.nikov04@gmail.com>
2024-06-03 16:37:07 +0300

    HM6

---
+++ Task6/Task6/Author.cs
@@ -0,0 +1,17 @@
+﻿namespace Task6
+{
+    public class Author
+    {
+        public Author(string fName, string lName, DateOnly bDate)
+        {
+            FirstName = fName;
+            LastName = lName;
+            BirthDate = bDate;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public List<Book> Books { get; set; } = new List<Book>();
+    }
+}
---
+++ Task6/Task6/Interfaces/IRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Task6.Interfaces
+{
+    public interface IRepository
+    {
+        void Save(Catalog catalog, string path);
+        Catalog Load(string path);
+    }
+}
